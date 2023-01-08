96253fdb2f16436b8d4d9d4db373b1445b4470f3
Claudio Cambra <developer@claudiocambra.com>
2023-01-08 01:38:00 +0100

    Move database path and installed plugins path to data folder
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- core/definitions.py
+++ core/definitions.py
@@ -2,5 +2,5 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-PLUGINS_PKG_DIR = "plugins"
-DATABASE_PATH = "core.db"
+PLUGINS_PKG_DIR = "data/installed-plugins"
+DATABASE_PATH = "data/core.db"
